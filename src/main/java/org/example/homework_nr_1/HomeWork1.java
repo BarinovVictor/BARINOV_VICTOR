520ed8bbfbe0f8d46629070851186024a554d8e2
Vito <victor.barinov@micb.md>
2023-03-17 18:32:57 +0200

    part 1

--- src/main/java/org/example/homework_nr_1/HomeWork1.java
+++ src/main/java/org/example/homework_nr_1/HomeWork1.java
@@ -1,8 +1,29 @@
 package org.example.homework_nr_1;
 
+import org.omg.CORBA.WStringSeqHelper;
+
 public class HomeWork1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        // Current World Population
+        long CWP = 7811080815L;
+        //Cars Produced This Year
+        int CP = 55235358;
+        //First Letter In Alphabet
+        char FL = 'A';
+        //String A = "A";
+        //Java Is Amazing - bool
+        boolean JIS = true;
+        // Pi
+        float PI = - 3.14159F;
+
+        System.out.println(CWP);
+        System.out.println(CP);
+        System.out.println(FL);
+        //System.out.println(A);
+        System.out.println(JIS);
+        System.out.println(PI);
+
     }
 }
