6791987b832664120ffd7632b8fa69543b39cd94
Vito <victor.barinov@micb.md>
2023-03-20 18:33:52 +0200

    Added homework number 1

--- src/main/java/org/example/homework_nr_1/HomeWork1.java
+++ src/main/java/org/example/homework_nr_1/HomeWork1.java
@@ -5,6 +5,7 @@ import org.omg.CORBA.WStringSeqHelper;
 public class HomeWork1 {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
         // Current World Population
         long CWP = 7811080815L;
@@ -12,7 +13,6 @@ public class HomeWork1 {
         int CP = 55235358;
         //First Letter In Alphabet
         char FL = 'A';
-        //String A = "A";
         //Java Is Amazing - bool
         boolean JIS = true;
         // Pi
@@ -21,9 +21,22 @@ public class HomeWork1 {
         System.out.println(CWP);
         System.out.println(CP);
         System.out.println(FL);
-        //System.out.println(A);
         System.out.println(JIS);
         System.out.println(PI);
+
+        double price = 12.5;
+        double tax = 21.5;
+        int quantity = 10;
+        double sum = 0;
+
+        System.out.println(price);
+        System.out.println(tax);
+        System.out.println(quantity);
+        System.out.println(sum);
+
+        double total = price + tax * quantity + sum;
+
+        System.out.println(total);
 
     }
 }
