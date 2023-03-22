a81f0eb1a139895dbc045bf41f4880fa00ae97ef
Vito <victor.barinov@micb.md>
2023-03-22 15:16:23 +0200

    homework_nr_2

--- src/main/java/org/example/homework_nr_2/HomeWork2.java
+++ src/main/java/org/example/homework_nr_2/HomeWork2.java
@@ -1,4 +1,49 @@
 package org.example.homework_nr_2;
 
 public class HomeWork2 {
+
+    public static void main(String[] args) {
+
+        int mounth = 10;
+
+        switch (mounth) {
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("September");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+            case 12:
+                System.out.println("December");
+                break;
+
+        }
+    }
 }
