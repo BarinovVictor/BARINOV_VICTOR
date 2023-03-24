1541f038224d7d836b81de8bd30d999b5792a7c1
Vito <victor.barinov@micb.md>
2023-03-24 18:06:26 +0200

    homework_nr_3 for check

--- src/main/java/org/example/homework_nr_3/Circle.java
+++ src/main/java/org/example/homework_nr_3/Circle.java
@@ -2,13 +2,33 @@ package org.example.homework_nr_3;
 
 public class Circle {
 
-    public static float calculateArea(float r){
-        return  (float) 3.14 * (r * r);
+    static double calculateArea(double r){
+        return  (double) 3.14 * (r * r);
     }
 
-    static void main(String[] args){
-        float radius = 5.15F;
+    public static void main(String[] args){
+        double radius = 5.5;
         System.out.println(calculateArea(radius));
+
+        TemperatureConverter Temperature = new TemperatureConverter();
+        System.out.println("41 degrees Celsius = " + Temperature.toFahrenheit(41) + " Fahrenheit");
+        System.out.println("41 degrees Fahrenheit = " + Temperature.toCelsius(41) + " Celsius");
     }
+
 }
 
+class TemperatureConverter{
+
+    static double toFahrenheit(double Celsius){
+        double p = 1.8;
+        return (Celsius * p) + 32;
+    }
+
+    static double toCelsius(double Fahrenheit){
+        double p = 1.8;
+        return (Fahrenheit - 32) / p;
+    }
+
+    public static void main(String[] args){}
+
+}
