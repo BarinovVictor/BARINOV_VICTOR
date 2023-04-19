ef57b7301f6a2789b492468d5db68a4976181bc2
Vito <victor.barinov@micb.md>
2023-04-19 16:26:45 +0300

    homework_nr_7

---
+++ src/main/java/org/example/homework_nr_7/Main.java
@@ -0,0 +1,10 @@
+package org.example.homework_nr_7;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student someStudent = new Student("Ivan","Dacia 33","c++",5, 15500);
+        System.out.println(someStudent);
+
+    }
+}
